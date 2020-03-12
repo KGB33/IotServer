acf7f92379df75f8bf1f2227e19da6ee69a413b4
kgb33 <github@bassingthwaite.org>
2020-03-12 16:32:04 -0500

    added append operation

--- IoTServer/db.py
+++ IoTServer/db.py
@@ -3,3 +3,10 @@ from tinydb import TinyDB, Query
 db = TinyDB("db.json")
 
 SensorQ = Query()
+
+
+def append(key, value):
+    def transform(doc):
+        doc[key].append(value)
+
+    return transform
