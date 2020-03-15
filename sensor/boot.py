4e5cb7256292ff5d7d35aee636c8b584bff2aa62
kgb33 <github@bassingthwaite.org>
2020-03-15 15:21:13 -0500

    set up wifi on boot

--- sensor/boot.py
+++ sensor/boot.py
@@ -7,3 +7,17 @@ esp.osdebug(None)
 import gc
 
 gc.collect()
+
+
+# Set Up Wifi
+from wifi_info import SSID, PASSWORD
+import network
+
+# Connect to Wifi
+sta = network.WLAN(network.STA_IF)
+sta.active(True)
+sta.connect(SSID, PASSWORD)
+
+# Shutdown Wifi server
+ap = network.WLAN(network.AP_IF)
+ap.active(False)
