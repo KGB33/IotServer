8b22fad59acd181295c5b2a4d38269a78c764652
kgb33 <github@bassingthwaite.org>
2020-03-15 15:21:30 -0500

    + network testing

--- sensor/test.py
+++ sensor/test.py
@@ -1,2 +1,12 @@
-for i in range(10):
-    print(i)
+sta = network.WLAN(network.STA_IF)
+ap = network.WLAN(network.AP_IF)
+
+print("Wifi Info\n")
+print("STA_IF")
+print("  Active: " + str(sta.active()))
+print("  Config: " + str(sta.ifconfig()))
+
+
+print("AP_IF")
+print("  Active: " + str(ap.active()))
+print("  Config: " + str(ap.ifconfig()))
