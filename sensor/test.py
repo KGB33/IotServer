9364f7703d6be5808328d7b9b236e8ad527d861e
kgb33 <github@bassingthwaite.org>
2020-03-17 20:58:22 -0500

    + sensor tests

--- sensor/test.py
+++ sensor/test.py
@@ -1,12 +1,27 @@
-sta = network.WLAN(network.STA_IF)
-ap = network.WLAN(network.AP_IF)
 
-print("Wifi Info\n")
-print("STA_IF")
-print("  Active: " + str(sta.active()))
-print("  Config: " + str(sta.ifconfig()))
+def test_network_info():
+    sta = network.WLAN(network.STA_IF)
+    ap = network.WLAN(network.AP_IF)
+
+    print("Wifi Info\n")
+    print("STA_IF")
+    print("  Active: " + str(sta.active()))
+    print("  Config: " + str(sta.ifconfig()))
 
 
-print("AP_IF")
-print("  Active: " + str(ap.active()))
-print("  Config: " + str(ap.ifconfig()))
+    print("AP_IF")
+    print("  Active: " + str(ap.active()))
+    print("  Config: " + str(ap.ifconfig()))
+
+def test_bme680():
+    try:
+        import main
+    except ImportError:
+        import _main as main
+    
+    print("Sensor Reading:")
+    print(main.get_reading())
+
+if __name__ == "__main__":
+    test_network_info()
+    test_bme680()
