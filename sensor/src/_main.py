74d722ad1e228027102e68325341c64c96b435db
kgb33 <github@bassingthwaite.org>
2020-03-17 20:59:02 -0500

    blacked

--- sensor/src/_main.py
+++ sensor/src/_main.py
@@ -7,13 +7,15 @@ import time
 i2c = busio.I2C(board.SCL, board.SDA)
 sensor = adafruit_bme680.Adafruit_BME680_I2C(i2c)
 
+
 def get_reading():
     return {
-        'tempature': sensor.temperature,
-        'gas': sensor.gas,
-        'humidity': sensor.humidity,
-        'pressure': sensor.pressure
+        "tempature": sensor.temperature,
+        "gas": sensor.gas,
+        "humidity": sensor.humidity,
+        "pressure": sensor.pressure,
     }
+
 
 if __name__ == "__main__":
     for _ in range(5):
--- sensor/test.py
+++ sensor/test.py
@@ -1,4 +1,3 @@
-
 def test_network_info():
     sta = network.WLAN(network.STA_IF)
     ap = network.WLAN(network.AP_IF)
@@ -8,19 +7,20 @@ def test_network_info():
     print("  Active: " + str(sta.active()))
     print("  Config: " + str(sta.ifconfig()))
 
-
     print("AP_IF")
     print("  Active: " + str(ap.active()))
     print("  Config: " + str(ap.ifconfig()))
+
 
 def test_bme680():
     try:
         import main
     except ImportError:
         import _main as main
-    
+
     print("Sensor Reading:")
     print(main.get_reading())
+
 
 if __name__ == "__main__":
     test_network_info()
