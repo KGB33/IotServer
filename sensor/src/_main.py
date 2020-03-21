d90d73479dfbf7b5e9e80944c65500e20a27142d
kgb33 <github@bassingthwaite.org>
2020-03-21 13:17:40 -0500

    added post URL for request module

--- sensor/src/_main.py
+++ sensor/src/_main.py
@@ -1,11 +1,16 @@
 import board
 import busio
+import microcontroller
 import adafruit_bme680
+import adafruit_requests
 import time
 
 
 i2c = busio.I2C(board.SCL, board.SDA)
 sensor = adafruit_bme680.Adafruit_BME680_I2C(i2c)
+
+SENSOR_ID = ''.join(str(x) for x in microcontroller.cpu.uid)
+POST_URL = "http://127.0.0.1:5000/api/v1/bme680/{}".format(SENSOR_ID)
 
 
 def get_reading():
@@ -16,8 +21,15 @@ def get_reading():
         "pressure": sensor.pressure,
     }
 
+def send_reading():
+    with adafruit_requests.post() as response:
+        pass
+
+
+
 
 if __name__ == "__main__":
-    for _ in range(5):
+    print("Url: ", POST_URL)
+    for _ in range(3):
         print(get_reading())
-        time.sleep(1)
+        time.sleep(0.5)
