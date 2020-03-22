a1260db0fa171a04237e254041db764c159dcba7
kgb33 <github@bassingthwaite.org>
2020-03-22 10:39:38 -0500

    added API endpoint url

--- sensor/src/_main.py
+++ sensor/src/_main.py
@@ -2,15 +2,17 @@ import board
 import busio
 import microcontroller
 import adafruit_bme680
-import adafruit_requests
+import requests
 import time
 
 
+# BME Sensor Setup
 i2c = busio.I2C(board.SCL, board.SDA)
 sensor = adafruit_bme680.Adafruit_BME680_I2C(i2c)
 
-SENSOR_ID = "".join(str(x) for x in microcontroller.cpu.uid)
-POST_URL = "http://127.0.0.1:5000/api/v1/bme680/{}".format(SENSOR_ID)
+# URL set up
+ESP8266_ID = "".join(str(x) for x in microcontroller.cpu.uid)
+POST_URL = "http://127.0.0.1:5000/api/v1/bme680/{}".format(ESP8266_ID)
 
 
 def get_reading():
@@ -22,9 +24,7 @@ def get_reading():
     }
 
 
-def send_reading():
-    with adafruit_requests.post() as response:
-        pass
+
 
 
 if __name__ == "__main__":
