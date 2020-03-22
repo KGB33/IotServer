0da35a070cfe2d709e0f20bdc7c8b002f3b583a4
kgb33 <github@bassingthwaite.org>
2020-03-22 10:37:18 -0500

    changed connection stuff

--- sensor/src/boot.py
+++ sensor/src/boot.py
@@ -13,11 +13,12 @@ gc.collect()
 from wifi_info import SSID, PASSWORD
 import network
 
-# Connect to Wifi
-sta = network.WLAN(network.STA_IF)
-sta.active(True)
-sta.connect(SSID, PASSWORD)
-
 # Shutdown Wifi server
 ap = network.WLAN(network.AP_IF)
 ap.active(False)
+
+# Connect to Wifi
+sta = network.WLAN(network.STA_IF)
+sta.active(True)
+if not sta.isconnected():
+    sta.connect(SSID, PASSWORD)
