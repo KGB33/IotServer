559ac9ea2d2625ecb9b2710d928351a69eb3e469
kgb33 <github@bassingthwaite.org>
2020-03-22 13:41:00 -0500

    reformatted bme680

--- sensor/src/_main.py
+++ sensor/src/_main.py
@@ -5,27 +5,26 @@ import adafruit_bme680
 import requests
 import time
 
-
-# BME Sensor Setup
-i2c = busio.I2C(board.SCL, board.SDA)
-sensor = adafruit_bme680.Adafruit_BME680_I2C(i2c)
-
-# URL set up
 ESP8266_ID = "".join(str(x) for x in microcontroller.cpu.uid)
-POST_URL = "http://127.0.0.1:5000/api/v1/bme680/{}".format(ESP8266_ID)
 
 
-def get_reading():
-    return {
-        "tempature": sensor.temperature,
-        "gas": sensor.gas,
-        "humidity": sensor.humidity,
-        "pressure": sensor.pressure,
-    }
+class bme680:
+    _i2c = busio.I2C(board.SCL, board.SDA)
+    _sensor = adafruit_bme680.Adafruit_BME680_I2C(_i2c)
+    POST_URL = "http://127.0.0.1:5000/api/v1/bme680/{}".format(ESP8266_ID)
+
+    @classmethod
+    def get_reading(cls):
+        return {
+            "tempature": cls._sensor.temperature,
+            "gas": cls._sensor.gas,
+            "humidity": cls._sensor.humidity,
+            "pressure": cls._sensor.pressure,
+        }
 
 
 if __name__ == "__main__":
-    print("Url: ", POST_URL)
+    print("Url: ", bme680.POST_URL)
     for _ in range(3):
-        print(get_reading())
+        print(bme680.get_reading())
         time.sleep(0.5)
