36467d80a6bc0329661be8ddd615aa3b6429c20e
kgb33 <github@bassingthwaite.org>
2020-03-22 10:01:36 -0500

    added mem test

--- sensor/test.py
+++ sensor/test.py
@@ -2,12 +2,16 @@ def test_network_info():
     sta = network.WLAN(network.STA_IF)
     ap = network.WLAN(network.AP_IF)
 
-    print("Wifi Info\n")
+    print("\nWifi Info")
     print("STA_IF")
+    print("  Is Connected: " + str(sta.isconnected()))
+    print("  Status: " + str(sta.status()))
     print("  Active: " + str(sta.active()))
     print("  Config: " + str(sta.ifconfig()))
 
     print("AP_IF")
+    print("  Is Connected: " + str(ap.isconnected()))
+    print("  Status: " + str(ap.status()))
     print("  Active: " + str(ap.active()))
     print("  Config: " + str(ap.ifconfig()))
 
@@ -18,10 +22,23 @@ def test_bme680():
     except ImportError:
         import _main as main
 
-    print("Sensor Reading:")
+    print("\nSensor Reading:")
     print(main.get_reading())
+
+
+def test_memory():
+    import gc
+
+    free = gc.mem_free()
+    used = gc.mem_alloc()
+    total = free + used
+    print("\nMemory:")
+    print("\tFree: ", free, "B")
+    print("\tUsed: ", used, "B")
+    print("\tTotal: ", total, "B")
 
 
 if __name__ == "__main__":
     test_network_info()
     test_bme680()
+    test_memory()
